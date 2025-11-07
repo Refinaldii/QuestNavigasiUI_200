9e16a61e85934b8681a810abb7b6a3f1895181b5
Refinaldii <refinaldi9786@gmail.com>
2025-11-07 10:20:14 +0700

    Nambah space button dan spacing

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -15,7 +15,8 @@ fun FormIsian(
             )
         }
     ) { isiRuang ->
-        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -25,30 +26,53 @@ fun FormIsian(
                 modifier = Modifier
                     .padding(top = 20.dp)
                     .width(width = 250.dp),
-                label = {Text(text = "Nama Lengkap")},
+                label = { Text(text = "Nama Lengkap") },
                 onValueChange = {},
             )
-            HorizontalDivider(modifier = Modifier
-                .padding(all = 20.dp)
-                .width(width = 250.dp), thickness = Thickness, color = Color.Red)
-            Row{
-                jenisK.forEach {
-                        item->
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp), thickness = Thickness, color = Color.Red
+            )
+            Row {
+                jenisK.forEach { item ->
                     Row(verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
                             selected = false,
-                            onClick = {item}
+                            onClick = { item }
                         )
                         Text(text = item)
                     }
                 }
             }
-            HorizontalDivider(modifier = Modifier
-                .padding(all = 20.dp)
-                .width(width = 250.dp),
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp),
                 thickness = 1.dp,
                 color = Color.Red
             )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = { Text(text = "Alamat") },
+                onValueChange = {},
+            )
+            Spacer(modifier = Modifier.height(height = 30.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 1f)
+                    .padding(all = 25.dp),
+                onClick = onSubmitBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.submit))
+            }
+        }
+    }
+
+
 
 
 
