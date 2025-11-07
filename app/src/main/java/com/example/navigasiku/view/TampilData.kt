597f728a777fbb84fd19e77a9ec47c0684ea80af
Refinaldii <refinaldi9786@gmail.com>
2025-11-07 09:54:58 +0700

    horizontal divider before gender selection

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -28,5 +28,10 @@ fun FormIsian(
                 label = {Text(text = "Nama Lengkap")},
                 onValueChange = {},
             )
-
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness, color = Color.Red)
+            Row{
+                jenisK.forEach {
+                        item->
 
